2547201ae123fc3b269c2b8bea89b8bc45709b87
Kimberly Simmons <kimberlysimmonsr@gmail.com>
2018-06-01 23:16:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTo(holidy, name){
+function happyHolidaysTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
